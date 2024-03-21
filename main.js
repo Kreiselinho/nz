67e814d1d33895e583ec2081d53b7361c2c81ec1
Kreiselinho <dominic.kreisel@student.uibk.ac.at>
2024-03-21 17:53:35 +0100

    Userbild als Vorschauicon verwendet

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([48.139722, 11.574444]).addTo(map);
-marker.bindPopup("<b>Hello New Zealand!</b><br>I am a popup.").openPopup();
+var marker = L.marker([-40.833333, 172.9]).addTo(map);
+marker.bindPopup("<b>Hello New Zealand!</b><br>Welcome to Abel Tasman National Park.").openPopup();
