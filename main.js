6a92f69066acb53d5cdc6598f17c954c4a71b2a4
Kreiselinho <dominic.kreisel@student.uibk.ac.at>
2024-04-11 15:55:52 +0200

    var durch let ersetzt

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([-40.833333, 172.9], 13);
+let map = L.map('map').setView([-40.833333, 172.9], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([-40.833333, 172.9]).addTo(map);
+let marker = L.marker([-40.833333, 172.9]).addTo(map);
 marker.bindPopup("<b>Hello New Zealand!</b><br>Welcome to Abel Tasman National Park.").openPopup();
