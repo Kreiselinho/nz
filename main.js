7a56f7c6bd5e8cf32ee612144cc2f11e59029cfd
Kreiselinho <dominic.kreisel@student.uibk.ac.at>
2024-04-11 16:05:39 +0200

    Variablen lat, lng und zoom definiert

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-let map = L.map('map').setView([-40.833333, 172.9], 13);
+let lat = -40.833333;
+let lng = 172.9;
+let zoom = 13;
+
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let marker = L.marker([-40.833333, 172.9]).addTo(map);
+let marker = L.marker([lat, lng]).addTo(map);
 marker.bindPopup("<b>Hello New Zealand!</b><br>Welcome to Abel Tasman National Park.").openPopup();
