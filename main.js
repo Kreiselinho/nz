287e894eea51412b6c55cbfb0472998454e93b26
Kreiselinho <dominic.kreisel@student.uibk.ac.at>
2024-04-11 17:37:36 +0200

    Marker als GeoJson eingebaut und Maßstab hinzugefügt

--- main.js
+++ main.js
@@ -12,15 +12,28 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let marker = L.marker([lat, lng]).addTo(map);
-    marker.bindPopup(`
-        <b>Welcome to Abel Tasman National Park!</b>
-        <ul>
-            <li>Breite: ${lat.toFixed(5)} </li>
-            <li>Länge: ${lng.toFixed(5)} </li>
-        </ul>
-    `).openPopup();
 
-    L.control.scale({
-        imperial:false
-    }).addTo(map);
+L.control.scale({
+    imperial: false
+}).addTo(map);
+
+let jsonPunkt = {
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [lng, lat]
+    },
+    "properties": {
+        "name": "Abel Tasman National Park"
+    }
+};
+
+L.geoJSON(jsonPunkt, {}).bindPopup(function (layer) {
+    return `
+    <b>${layer.feature.properties.name}</b>
+    <ul>
+        <li>Breite: ${layer.feature.geometry.coordinates[1].toFixed(5)} </li>
+        <li>Länge: ${layer.feature.geometry.coordinates[0].toFixed(5)} </li>
+    </ul>
+`;
+}).addTo(map);
